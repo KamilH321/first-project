33c4ca20881cdd0d5d7a84f107edf47f29d9c8ac
KamilH321 <98100553+KamilH321@users.noreply.github.com>
2026-03-21 19:06:06 +0300

    Add necessary libs and plugins

--- FirstProject/app/build.gradle.kts
+++ FirstProject/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp.plugin)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -42,6 +44,8 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+
+    // Compose
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -49,6 +53,31 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Navigation
+    implementation(libs.compose.navigation)
+
+    // Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines)
+
+    // Room
+    implementation(libs.room)
+    ksp(libs.room.compiler)
+
+    // Hilt
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)  // Используем ksp для Hilt
+    implementation(libs.javax.inject)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
